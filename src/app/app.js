930c13d887159e534ee674f26456e31886174e09
Estebanmual <esteban.munozal@gmail.com>
2022-02-17 13:15:50 -0600

    On branch feature/add-new-task 	modified:   src/app/app.js 	new file:   src/app/utils/crud.js 	modified:   src/app/utils/display-task.js 	modified:   src/app/utils/tasks.js 	modified:   src/template.html    Added the feature to add new tasks to the list

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,14 @@
 import displayTasks from './utils/display-task.js';
+import CRUD from './utils/crud.js';
+
+const inputAdd = document.querySelector('#add-input');
 
 displayTasks();
+
+inputAdd.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    CRUD.addTaskToList();
+    window.location.reload();
+  }
+});
---
+++ src/app/utils/crud.js
@@ -0,0 +1,20 @@
+import getTasks from './tasks.js';
+
+function Task(index, completed, description) {
+  this.index = index;
+  this.completed = completed;
+  this.description = description;
+}
+
+export default class CRUD {
+  static addTaskToList() {
+    const tasks = getTasks();
+    const tasksLength = tasks.length;
+    const descriptionNewTask = document.querySelector('.add-input').value;
+    const indexNewTask = tasksLength;
+    const completedNewTask = false;
+    const task = new Task(indexNewTask, completedNewTask, descriptionNewTask);
+    tasks.push(task);
+    localStorage.setItem('localTasks', JSON.stringify(tasks));
+  }
+}
