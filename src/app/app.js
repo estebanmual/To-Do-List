3ebe4a041b20343ab08c523fb8b68a61460ced21
Estebanmual <esteban.munozal@gmail.com>
2022-02-19 13:17:00 -0600

    Added event when checkbox change and also change the local storage everytime something happen.

--- src/app/app.js
+++ src/app/app.js
@@ -2,9 +2,11 @@ import displayTasks from './utils/display-task.js';
 import CRUD from './utils/crud.js';
 import checkMark from '../assets/check-mark.svg';
 import editIcon from '../assets/editIcon.svg';
+import checkboxesFunc from './utils/update-completed.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   displayTasks();
+  checkboxesFunc();
   const inputAdd = document.querySelector('#add-input');
   inputAdd.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
---
+++ src/app/utils/update-completed.js
@@ -0,0 +1,38 @@
+import getTasks from './tasks.js';
+
+export default function checkboxesFunc() {
+  const checkboxes = document.querySelectorAll('.checkbox');
+  const tasks = getTasks();
+
+  for (let i=0; i < tasks.length; i += 1) {
+    if (tasks[i].completed === true ) {
+           const elementIndex = tasks[i].index;
+           const taskToAddClass = document.getElementById(`${elementIndex}`);
+           taskToAddClass.checked = true;
+           taskToAddClass.nextElementSibling.classList.add('checked-box')
+
+    }
+  }
+  checkboxes.forEach((checkboxes) => {
+    checkboxes.addEventListener('change', (g) => {
+      if (g.target.checked === true) {
+        g.target.nextSibling.classList.add('checked-box');
+        for (let i = 0; i < tasks.length; i += 1){
+          if (Number(g.target.id) === tasks[i].index) {
+            tasks[i].completed = true;
+            localStorage.setItem('localTasks', JSON.stringify(tasks));
+          }
+        };
+      } else {
+        if (g.target.checked === false) {
+          g.target.nextSibling.classList.remove('checked-box');
+          for (let i = 0; i < tasks.length; i += 1){
+            if (Number(g.target.id) === tasks[i].index) {
+              tasks[i].completed = false;
+              localStorage.setItem('localTasks', JSON.stringify(tasks));
+            }
+          };
+      }
+    };
+  });
+})}
